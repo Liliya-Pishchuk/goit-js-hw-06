c1b55c48e77b2f7474ed1b2bee8534cc5546dad8
Liliya-Pishchuk <metelyck@gmail.com>
2022-07-12 22:43:11 +0300

    task-2 answer

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsListEl = document.querySelector("#ingredients");
+
+const newAr = (element) => {
+  return element.map((el) => {
+    const ingredientItemEl = document.createElement("li");
+    ingredientItemEl.textContent = el;
+    ingredientItemEl.classList.add("item");
+    return ingredientItemEl;
+  });
+};
+
+const newIngredientsListEl = newAr(ingredients);
+ingredientsListEl.append(...newIngredientsListEl);
