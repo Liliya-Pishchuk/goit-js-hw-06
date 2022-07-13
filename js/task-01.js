40f9f85a02f91efa03f6d6385ed90fab5e5e78f5
Liliya-Pishchuk <metelyck@gmail.com>
2022-07-13 21:01:25 +0300

    corrected

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,19 @@
-const categoriesEl = document.querySelectorAll(".item");
-const numberOfCategory = categoriesEl.length;
-console.log(`Number of categories: ${numberOfCategory}`);
+// Напиши скрипт, який:
 
-[...categoriesEl].forEach((element) => {
-  const categoryName = element.querySelector("h2");
-  const categoryAmount = element.querySelectorAll("li");
+// Порахує і виведе в консоль кількість категорій в ul#categories,
+// тобто елементів li.item.
 
-  console.log(`Category: ${categoryName.textContent}`);
-  console.log(`Elements: ${categoryAmount.length}`);
+// // Для кожного элемента li.item у спику ul#categories,
+// знайде і виведе в консоль текст заголовку елемента(тегу < h2 >)
+// і кількість елементів в категорії(усіх < li >, вкладених в нього).
+
+const categoryItemEl = document.querySelectorAll(".item");
+console.log(`Number of categories: ${categoryItemEl.length}`);
+
+[...categoryItemEl].forEach((category) => {
+  const categoryNameEl = category.querySelector("h2");
+  console.log(`Category: ${categoryNameEl.textContent}`);
+
+  const categoryAmountEl = category.querySelectorAll("li");
+  console.log(`Elements: ${categoryAmountEl.length}`);
 });
